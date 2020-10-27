2d8a0ba476592fe9443b8f05697fbb6117630868
Sascha Sallès <sascha.salles@ynov.com>
2020-10-27 15:27:03 +0100

    rename images

--- lib/widgets/add_custom_btn.dart
+++ lib/widgets/add_custom_btn.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-
-
 class addButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Card(
-        child: Text("test"),
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Text(""),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.all(10),
       ),
     );
   }
