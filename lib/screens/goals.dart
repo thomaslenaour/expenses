3bd8a0fc6e6bf33d91fdb9740be7e53caab88308
Thomas Le Naour <lenaourthomas@gmail.com>
2020-10-27 15:00:32 +0100

    add main title and second title on goals page

--- lib/screens/goals.dart
+++ lib/screens/goals.dart
@@ -4,8 +4,26 @@ class GoalsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('Hello World'),
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Objectifs',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Text(
+            'Vos objectifs',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
       ),
     );
   }
